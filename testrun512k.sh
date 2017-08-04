d0d07376e87d5d5bd3f302c9c0c571cd65b7ed2d
Hollandb1 <Hollandb1@wit.edu>
2017-08-03 20:35:46 -0400

    Graphs n stufffffff

--- testrun512k.sh
+++ testrun512k.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 printf "Copying data file so as to not ruin the original...\n"
-cp -f -v ./data_512k_master data_512k
+cp -f -v data_512k_master data_512k
 printf "Compiling the program to follow the latest update...\n"
 gcc -o mtfs mtfs.c -lpthread
 read -s -t 2 -p "Ready to run? (y/n): " -n 1 response
@@ -10,9 +10,11 @@ if [[ $response == 'y' || $response == 'Y' ]]; then
     for num_threads in 1 2 4 8 16
     do
         if [[ $num_threads == 1 ]]; then
-            ./mtfs -t 10 -n $num_threads data_512k
+        	cp -f data_512k_master data_512k
+            ./mtfs -t 1 -n $num_threads data_512k
         else
-            ./mtfs -t 10 -n $num_threads data_512k
+            cp -f data_512k_master data_512k
+            ./mtfs -t 1 -n $num_threads data_512k
         fi
     done
 fi
